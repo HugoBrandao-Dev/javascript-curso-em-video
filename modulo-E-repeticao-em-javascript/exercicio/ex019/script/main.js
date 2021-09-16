3cf8c1e9198fe1b93e644160e99f750f258d15f4
HugoBrandao <hugobrandao.dev@gmail.com>
2021-09-16 13:38:14 -0300

    Modificado o exercicio de contagem, para que haja uma maior semelhanca com o do professor.

--- modulo-E-repeticao-em-javascript/exercicio/ex019/script/main.js
+++ modulo-E-repeticao-em-javascript/exercicio/ex019/script/main.js
@@ -1,43 +1,51 @@
 // Elementos DOM
-var btnContar = document.getElementById('btn-contar')
+let btnContar = document.getElementById('btn-contar')
 
 // Eventos DOM
 btnContar.addEventListener('click', contar)
 
 function contar() {
 	// Elementos DOM
-	var inputInicio = document.getElementsByName('input-inicio')[0]
-	var inputFim = document.getElementsByName('input-fim')[0]
-	var inputPasso = document.getElementsByName('input-passo')[0]
-	var divSaida = document.querySelector('div#saida')
+	let inputInicio = document.getElementsByName('input-inicio')[0]
+	let inputFim = document.getElementsByName('input-fim')[0]
+	let inputPasso = document.getElementsByName('input-passo')[0]
+	let divSaida = document.querySelector('div#saida')
 	
 	// Variáveis
 	if (inputInicio.value.length == 0 || inputFim.value.length == 0 || inputPasso.value.length == 0) {
 		alert('ERRO: Valores ausentes. Verifique os valores novamente.')
 	} else {
-		var inicio = Number(inputInicio.value)
-		var fim = Number(inputFim.value)
+		let inicio = Number(inputInicio.value)
+		let fim = Number(inputFim.value)
+		let passo
 		if (inputPasso.value <= 0) {
 			window.alert('ERRO: O valor do passo é inválido. Passo assumirá valor de 1 (padrão).')
-			var passo = 1
+			passo = 1
 		} else {
-			var passo = Number(inputPasso.value)
+			passo = Number(inputPasso.value)
 		}
 		
 		// Elementos criados
-		var pContagem = document.createElement('p')
+		let pContagem = document.createElement('p')
+		pContagem.style.lineHeight = '30px'
 		if (fim > inicio) {
-			for (var cont = inicio; cont <= fim; cont += passo) {
-				pContagem.innerHTML += `${ cont } &rarr; `
+			for (let cont = inicio; cont <= fim; cont += passo) {
+				pContagem.innerHTML += `${ cont } \u{1F449} `
 			}
 		} else {
-			for (var cont = inicio; cont >= fim; cont -= passo) {
-				pContagem.innerHTML += `${ cont } &rarr; `
+			for (let cont = inicio; cont >= fim; cont -= passo) {
+				pContagem.innerHTML += `${ cont } \u{1F449} `
 			}
 		}
-		pContagem.innerText += ' FIM'
+		pContagem.innerText += ' \u{1F3C1}'
 		
+		// Limpando os campos
 		divSaida.innerText = ''
+		inputInicio.value = ''
+		inputFim.value = ''
+		inputPasso.value = ''
+
 		divSaida.appendChild(pContagem)
+
 	}
 }
